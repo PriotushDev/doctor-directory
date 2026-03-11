0a39ec92429326b5a5f43289e4f4b91364da88af
Priyotosh <ai.priyobabu@gmail.com>
2026-03-11 22:48:01 +0600

    doctor_chamber and appointment model & migration append

---
+++ database/migrations/2026_03_11_164508_create_doctor_chambers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctor_chambers', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('doctor_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('hospital_id')->constrained()->cascadeOnDelete();
+
+            $table->string('day');
+            $table->time('start_time');
+            $table->time('end_time');
+
+            $table->integer('fee')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctor_chambers');
+    }
+};
