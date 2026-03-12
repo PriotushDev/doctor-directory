702db66d73ea65d8e752e8c1717a108e9c69b176
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 10:53:15 +0600

    set routes/api file

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\DivisionController;
-use App\Http\Controllers\Api\DistrictController;
-use App\Http\Controllers\Api\HospitalController;
-use App\Http\Controllers\Api\DoctorController;
-use App\Http\Controllers\Api\AppointmentController;
+
 
 Route::get('/test', function () {
     return response()->json([
@@ -13,8 +9,4 @@ Route::get('/test', function () {
     ]);
 });
 
-Route::apiResource('divisions', DivisionController::class);
-Route::apiResource('districts', DistrictController::class);
-Route::apiResource('hospitals', HospitalController::class);
-Route::apiResource('doctors', DoctorController::class);
-Route::apiResource('appointments', AppointmentController::class);
+
