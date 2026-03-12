034567f8aefe270b0ac5c05e34a0367ef30b0fa3
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 10:16:22 +0600

    doctor model set

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'specialty_id',
+        'hospital_id',
+        'degree',
+        'experience',
+        'phone',
+        'email',
+        'bio',
+        'photo'
+    ];
+
+    public function specialty()
+    {
+        return $this->belongsTo(Specialty::class);
+    }
+
+    public function hospital()
+    {
+        return $this->belongsTo(Hospital::class);
+    }
+
+    public function chambers()
+    {
+        return $this->hasMany(DoctorChamber::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
 }
