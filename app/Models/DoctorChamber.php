fb721c3bd8389d150a917eaa01c2ce467d895b91
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 10:19:12 +0600

    doctor-chamber model set

--- app/Models/DoctorChamber.php
+++ app/Models/DoctorChamber.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class DoctorChamber extends Model
 {
-    //
+    protected $fillable = [
+        'doctor_id',
+        'hospital_id',
+        'day',
+        'start_time',
+        'end_time',
+        'fee'
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function hospital()
+    {
+        return $this->belongsTo(Hospital::class);
+    }
 }
