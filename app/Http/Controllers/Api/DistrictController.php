5acc0cc2a99585ff205b2089d43286143a72c16b
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 12:51:57 +0600

    write district controller code and testing postman successfully

--- app/Http/Controllers/Api/DistrictController.php
+++ app/Http/Controllers/Api/DistrictController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\District;
+
 class DistrictController extends Controller
 {
     /**
