fd5b98e247a2ed634cd3e313e8cbc0ea26ecff25
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 10:26:59 +0600

    appointment model set

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    protected $fillable = [
+        'doctor_id',
+        'user_id',
+        'appointment_date',
+        'appointment_time',
+        'status',
+        'notes'
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
