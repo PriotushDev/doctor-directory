cd52f57857668d8e5986fde2bac40117fb43423a
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 13:33:51 +0600

    write Hospital controller code  and perfect testing postman

--- app/Http/Controllers/Api/HospitalController.php
+++ app/Http/Controllers/Api/HospitalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Hospital;
 
 class HospitalController extends Controller
 {
@@ -12,7 +13,9 @@ class HospitalController extends Controller
      */
     public function index()
     {
-        //
+        $hospitals = Hospital::with('district')->latest()->get();
+
+        return response()->json($hospitals);
     }
 
     /**
@@ -20,30 +23,57 @@ class HospitalController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $hospital = Hospital::create([
+            'name' => $request->name,
+            'district_id' => $request->district_id,
+            'address' => $request->address,
+            'phone' => $request->phone
+        ]);
+
+        return response()->json([
+            'message' => 'Hospital created successfully',
+            'data' => $hospital
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $hospital = Hospital::with('district')->findOrFail($id);
+
+        return response()->json($hospital);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $hospital = Hospital::findOrFail($id);
+
+        $hospital->update([
+            'name' => $request->name,
+            'district_id' => $request->district_id,
+            'address' => $request->address,
+            'phone' => $request->phone
+        ]);
+
+        return response()->json([
+            'message' => 'Hospital updated successfully'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        Hospital::destroy($id);
+
+        return response()->json([
+            'message' => 'Hospital deleted successfully'
+        ]);
     }
 }
