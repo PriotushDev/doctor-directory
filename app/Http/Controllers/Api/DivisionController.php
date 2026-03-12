2d6226c19f3a59eedb82aec2216e79b954fe1e4e
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 11:15:35 +0600

    division controller api code set and testing successfully

--- app/Http/Controllers/Api/DivisionController.php
+++ app/Http/Controllers/Api/DivisionController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Division;
+
 class DivisionController extends Controller
 {
     /**
@@ -12,7 +14,7 @@ class DivisionController extends Controller
      */
     public function index()
     {
-        //
+        return Division::latest()->get();
     }
 
     /**
@@ -20,30 +22,49 @@ class DivisionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $division = Division::create([
+            'name' => $request->name
+        ]);
+
+        return response()->json([
+            'message' => 'Division created successfully',
+            'data' => $division
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        return Division::findOrFail($id);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $division = Division::findOrFail($id);
+
+        $division->update([
+            'name' => $request->name
+        ]);
+
+        return response()->json([
+            'message' => 'Division updated successfully'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        Division::destroy($id);
+
+        return response()->json([
+            'message' => 'Division deleted successfully'
+        ]);
     }
 }
