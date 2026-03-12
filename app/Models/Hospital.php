2e2c1d1ea682c57158c7b37855deb2501d627825
Priyotosh <ai.priyobabu@gmail.com>
2026-03-12 10:10:05 +0600

    hospital model set

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hospital extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'district_id',
+        'address',
+        'phone'
+    ];
+
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
+
+    public function doctors()
+    {
+        return $this->hasMany(Doctor::class);
+    }
 }
